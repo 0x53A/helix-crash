414b61cf6b494136c0f845c08af3c18279f61594
lrieger <lr@live.de>
2015-09-08 19:25:05 +0200

    add

---
+++ HelixCrash/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelixCrash
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var material = MaterialHelper.CreateMaterial(Colors.Aqua);
+
+            var builder = new MeshBuilder();
+            builder.AddTriangle(new Point3D(0, 0, 0), new Point3D(0, 10, 100), new Point3D(100, 0, 0));
+            var mesh = builder.ToMesh();
+            var geo = new GeometryModel3D { Geometry = mesh, Material = material , Transform = null};
+            var model = new ModelVisual3D { Content = geo, Transform = null };
+            _visual.Children.Add(model);
+        }
+    }
+}
